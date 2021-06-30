20b21501a72a22b0721d349696c1ebfce233e130
Asif Shikder <asif.shikder.ar@gmail.com>
2021-06-30 20:26:26 +0600

    first commit

---
+++ Identify/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Identify.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identify.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _logger = logger;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        [Authorize]
+        public IActionResult Index()
+        {
+            var user = GetCurrentUserAsync().Result;
+            return View(user);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        private Task<ApplicationUser> GetCurrentUserAsync() => userManager.GetUserAsync(HttpContext.User);
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
